40a99a2dfd423bf72e923982279677cb99501caf
Nik White <d00345747@dmail.dixie.edu>
2020-10-08 13:38:51 -0600

    made changes to .tf file

--- tfaws.tf
+++ tfaws.tf
@@ -12,7 +12,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f2b111fdc1647918"
+resource "aws_instance" "terraform2" {
+  ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
 }
+
