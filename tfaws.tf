3d5b9e3081791b3dd749ad35da4ab2bef8f81ba4
Nik White <d00345747@dmail.dixie.edu>
2020-10-08 13:09:31 -0600

    added configuration file

---
+++ tfaws.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0f2b111fdc1647918"
+  instance_type = "t2.micro"
+}
